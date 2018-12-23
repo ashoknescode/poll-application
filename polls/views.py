6d15dd128899361befa1ba735e1b07fbc77bbb48
ashoknescode <ashok.kumar@nescode.com>
2018-12-23 15:01:12 +0530

    [Update]admin page

--- polls/views.py
+++ polls/views.py
@@ -16,9 +16,6 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-        #Return the last five published questions.
-		# return Question.objects.order_by('-pub_date')[:5]
-
 		return Question.objects.filter(
 		pub_date__lte=timezone.now()
 	).order_by('-pub_date')[:5]
